17843a56f27e2782aba6f3345dc05c436970eaf5
RiieMa <marydiouf09@gmail.com>
2022-06-23 01:39:02 +0200

    Authentification

--- sunu-littoral/database/migrations/2014_10_12_000000_create_users_table.php
+++ sunu-littoral/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastName');
+            $table->string('tel');
+            $table->string('adresse');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
