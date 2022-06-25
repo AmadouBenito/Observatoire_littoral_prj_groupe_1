3d16264c878c6f2701b35aec8c3efa2ada2f6782
Amadou DIOP <beni.diop.95@gmail.com>
2022-06-25 13:20:45 +0000

    Fixing the problem of auth...

--- sunu-littoral/app/Models/User.php
+++ sunu-littoral/app/Models/User.php
@@ -1,46 +1,17 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-/**
- * Class User
- * 
- * @property int $id
- * @property string $name
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property int $roles_idroles
- * 
- * @property Collection|Actualite[] $actualites
- * @property Collection|Csv[] $csvs
- * @property Collection|Image[] $images
- * @property Collection|Meteo[] $meteos
- * @property Collection|Pdf[] $pdfs
- * @property Collection|Role[] $roles
- * @property Collection|Texte[] $textes
- * @property Collection|Video[] $videos
- *
- * @package App\Models
- */
-class User extends Model
+class User extends Authenticatable
 {
-	protected $table = 'users';
+    use HasApiTokens, HasFactory, Notifiable;
 
-	protected $casts = [
-		'roles_idroles' => 'int'
-	];
     /**
      * The attributes that are mass assignable.
      *
@@ -58,52 +29,22 @@ class User extends Model
 
     ];
 
-	protected $dates = [
-		'email_verified_at'
-	];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-	protected $hidden = [
-		'password',
-		'remember_token'
-	];
-
-	public function actualites()
-	{
-		return $this->hasMany(Actualite::class, 'users_id');
-	}
-
-	public function csvs()
-	{
-		return $this->hasMany(Csv::class, 'users_id');
-	}
-
-	public function images()
-	{
-		return $this->hasMany(Image::class, 'users_id');
-	}
-
-	public function meteos()
-	{
-		return $this->hasMany(Meteo::class, 'users_id');
-	}
-
-	public function pdfs()
-	{
-		return $this->hasMany(Pdf::class, 'users_id');
-	}
-
-	public function roles()
-	{
-		return $this->hasMany(Role::class, 'users_id');
-	}
-
-	public function textes()
-	{
-		return $this->hasMany(Texte::class, 'users_id');
-	}
-
-	public function videos()
-	{
-		return $this->hasMany(Video::class, 'users_id');
-	}
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
