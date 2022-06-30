b7214e0213e71dd02e4d0101befa31e3dc1adafe
Amadou DIOP <beni.diop.95@gmail.com>
2022-06-30 14:58:45 +0000

    Modification du header

--- sunu-littoral/resources/views/site/layouts/app.blade.php
+++ sunu-littoral/resources/views/site/layouts/app.blade.php
@@ -41,18 +41,95 @@
 
 <body>
 
-    @yield('menu-mobile')
-    @yield('actualite')
+    <!--====== OFFCANVAS MENU PART START ======-->
+
+    <div class="binduz-er-news-off_canvars_overlay"></div>
+    <div class="binduz-er-news-offcanvas_menu">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="binduz-er-news-offcanvas_menu_wrapper">
+                        <div class="binduz-er-news-canvas_close">
+                            <a href="javascript:void(0)"><i class="fal fa-times"></i></a>
+                        </div>
+                        <div class="binduz-er-news-header-social">
+                            <ul class="text-center">
+                                <li><a href="#">facebook</a></li>
+                                <li><a href="#">Twitter</a></li>
+                                <li><a href="#">Skype</a></li>
+                            </ul>
+                        </div>
+                        <div id="menu" class="text-left ">
+                            <ul class="binduz-er-news-offcanvas_main_menu">
+                                <li class="binduz-er-news-menu-item-has-children binduz-er-news-active">
+                                    <a href="#">Home</a>
+                                    <ul class="binduz-er-news-sub-menu">
+                                        <li><a href="#html">Home 1</a></li>
+                                        <li><a href="#2.html">Home 2</a></li>
+                                        <li><a href="#3.html">Home 3</a></li>
+                                        <li><a href="#4.html">Home 4</a></li>
+                                        <li><a href="#5.html">Home 5</a></li>
+                                        <li><a href="#6.html">Home 6</a></li>
+                                        <li><a href="#7.html">Home 7</a></li>
+                                        <li><a href="#8.html">Home 8</a></li>
+                                        <li><a href="#9.html">Home 9</a></li>
+                                        <li><a href="#10.html">Home 10</a></li>
+                                        <li><a href="#11.html">Home 11</a></li>
+                                        <li><a href="#12.html">Home 12</a></li>
+                                    </ul>
+                                </li>
+                                <li class="binduz-er-news-menu-item-has-children">
+                                    <a href="archived.html">Archived </a>
+                                </li>
+                                <li class="binduz-er-news-menu-item-has-children">
+                                    <a href="author.html">Author</a>
+                                </li>
+                                <li class="binduz-er-news-menu-item-has-children">
+                                    <a href="#"> Pages</a>
+                                    <ul class="binduz-er-news-sub-menu">
+                                        <li><a href="blog-details-1.html">Blog Details 1</a></li>
+                                        <li><a href="blog-details-2.html">Blog Details 2</a></li>
+                                    </ul>
+                                </li>
+                                <li class="binduz-er-news-menu-item-has-children">
+                                    <a href="about-us.html"> About</a>
+                                </li>
+                                <li class="binduz-er-news-menu-item-has-children">
+                                    <a href="contact.html"> Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="binduz-er-news-offcanvas_footer">
+                            <div class="binduz-er-news-logo text-center mb-30 mt-30">
+                                <a href="#html">
+                                    <img src="{{asset('site/images/logo.png')}}" alt="">
+                                </a>
+                            </div>
+                            <p>I’m Michal Škvarenina, a multi-disciplinary designer currently working at Wild and as a freelance designer.</p>
+                            <ul>
+                                <li><i class="fas fa-phone"></i> +212 34 45 45 98</li>
+                                <li><i class="fas fa-home"></i> Angle Bd Abdelmoumen & rue soumaya, Résidence</li>
+                                <li><i class="fas fa-envelope"></i> hello@example.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== OFFCANVAS MENU PART ENDS ======-->
+
     <!--====== SEARCH PART START ======-->
 
     <div class="binduz-er-news-search-box">
         <div class="binduz-er-news-search-header">
-            <div class=" container mt-60">
+            <div class="container mt-60">
                 <div class="row">
-                    <div class=" col-6">
-                        <img src="{{asset('site/images/logo-4.png')}}" alt=""> <!-- search title -->
+                    <div class="col-6">
+                        <img src="{{asset('assets/images/logo-4.png')}}" alt=""> <!-- search title -->
                     </div>
-                    <div class=" col-6">
+                    <div class="col-6">
                         <div class="binduz-er-news-search-close float-end">
                             <button class="binduz-er-news-search-close-btn">Close <span></span><span></span></button>
                         </div> <!-- search close -->
@@ -61,13 +138,13 @@
             </div> <!-- container -->
         </div> <!-- search header -->
         <div class="binduz-er-news-search-body">
-            <div class=" container">
+            <div class="container">
                 <div class="row">
-                    <div class=" col-lg-12">
+                    <div class="col-lg-12">
                         <div class="binduz-er-news-search-form">
                             <form action="#">
                                 <input type="text" placeholder="Search for Products">
-                                <button><i class="far fa-search"></i></button>
+                                <button><i class="fa fa-search"></i></button>
                             </form>
                         </div>
                     </div>
@@ -78,58 +155,99 @@
 
     <!--====== SEARCH PART ENDS ======-->
 
+    <!--====== BINDUZ TOP HEADER PART START ======-->
+
+    <div class="binduz-er-top-header-area">
+        <div class="binduz-er-bg-cover"></div>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="binduz-er-topbar-headline">
+                        <p><span><i class="fas fa-bolt"></i> Trending News:</span> <a href="#">Miranda halim was viral after finish his workout.</a></p>
+                        <p><span><i class="fas fa-bolt"></i> Trending News:</span> <a href="#">Celebrating Asian Pacific American art and cultures</a></p>
+                        <p><span><i class="fas fa-bolt"></i> Trending News:</span> <a href="#">Honoring Asian Pacific American Heritage Month at Google</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="binduz-er-topbar-social d-flex justify-content-end align-items-center">
+                        <div class="binduz-er-news-social">
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-behance"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="binduz-er-news-subscribe-btn">
+                            <a class="binduz-er-main-btn" href="#">Subscribe</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== BINDUZ TOP HEADER PART ENDS ======-->
+
     <!--====== BINDUZ HEADER PART START ======-->
 
-    <header class="binduz-er-header-area binduz-er-header-area-3">
+    <header class="binduz-er-header-area">
         <div class="binduz-er-header-nav">
-            <div class=" container-fluid p-0">
+            <div class="container">
                 <div class="row">
-                    <div class=" col-lg-12">
+                    <div class="col-lg-12">
                         <div class="navigation">
                             <nav class="navbar navbar-expand-lg">
-                                <div class="navbar-brand logo"><a href="index.html"><img src="{{asset('site/images/logo-5.png')}}" alt=""></a></div> <!-- logo -->
+                                <div class="navbar-brand logo"><a href="#html"><img src="{{asset('site/images/logo.png')}}" alt=""></a></div> <!-- logo -->
                                 <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-                                    <ul class="navbar-nav mr-auto">
+                                    <ul class="navbar-nav m-auto">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#accueil">Accueil </a>
+                                        </li>
                                         <li class="nav-item active">
-                                            <a class="binduz-er-nav-link" href="index.html">Accueil</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="archived.html">Presentation <i class="fa fa-angle-down"></i></a>
+                                            <a class="nav-link" href="#html">Préserntation <i class="fa fa-angle-down"></i></a>
                                             <ul class="sub-menu">
-                                                <li><a href="blog-details-1.html">historique</a></li>
-                                                <li><a href="blog-details-2.html">mission</a></li>
+                                                <li><a href="#html">Historique</a></li>
+                                                <li><a href="#2.html">Mission</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item active">
+                                            <a class="nav-link" href="#html">Publication <i class="fa fa-angle-down"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="#html">Rapports</a></li>
+                                                <li><a href="#2.html">Météo</a></li>
+                                                <li><a href="#2.html">Appel d'offre</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item active">
+                                            <a class="nav-link" href="#html">Média <i class="fa fa-angle-down"></i></a>
+                                            <ul class="sub-menu">
+                                                <li><a href="#html">Vidéo</a></li>
+                                                <li><a href="#2.html">Image</a></li>
+                                                <li><a href="#2.html">Audio</a></li>
                                             </ul>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" href="archived.html">Publication <i class="fa fa-angle-down"></i></a>
-                                            <ul class="sub-menu">
-                                                <li><a href="blog-details-1.html">rapport</a></li>
-                                                <li><a href="blog-details-2.html">meteo</a></li>
-                                            </ul>
+                                            <a class="nav-link" href="#accueil">Projets </a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" href="#">Media<i class="fa fa-angle-down"></i></a>
-                                            <ul class="sub-menu">
-                                                <li><a href="blog-details-1.html">video</a></li>
-                                                <li><a href="blog-details-2.html">image</a></li>
-                                                <li><a href="blog-details-2.html">audio</a></li>
-                                            </ul>
+                                            <a class="nav-link" href="#accueil">A propos </a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" href="#">Projet<i class="fa fa-angle-down"></i></a>
-                                            <ul class="sub-menu">
-                                                <li><a href="blog-details-1.html">appel offre</a></li>
-                                                <li><a href="blog-details-2.html">projet</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="about-us.html">A Propos</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="contact.html">Contactez-Nous</a>
+                                            <a class="nav-link" href="#accueil">Contactez-nous </a>
                                         </li>
                                     </ul>
                                 </div> <!-- navbar collapse -->
+                                <div class="binduz-er-navbar-btn d-flex">
+                                    <div class="binduz-er-widget d-flex">
+                                        <a class="binduz-er-news-search-open" href="#"><i class="far fa-search"></i></a>
+                                        <a href="#"><i class="far fa-user"></i></a>
+                                    </div>
+                                    <span class="binduz-er-toggle-btn binduz-er-news-canvas_open d-block d-lg-none">
+                                        <i class="fal fa-bars"></i>
+                                    </span>
+                                </div>
                             </nav>
                         </div> <!-- navigation -->
                     </div>
@@ -139,55 +257,6 @@
     </header>
 
     <!--====== BINDUZ HEADER PART ENDS ======-->
-
-    @yield('contenu')
-
-    <!--====== BINDUZ POPULER NEWS PART ENDS ======-->
-
-    <!--====== BINDUZ FOOTER 3  PART START ======-->
-
-
-    <div class="binduz-er-footer-copyright-area">
-        <div class=" container">
-            <div class="row align-items-center">
-                <div class=" col-lg-4">
-                    <div class="binduz-er-copyright-text">
-                        <p>Copyright By@<span>QuomodoTheme</span> - 2021</p>
-                    </div>
-                </div>
-                <div class=" col-lg-8">
-                    <div class="binduz-er-copyright-menu float-lg-end float-none">
-                        <ul>
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Accessibility & CC</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Studio Tours</a></li>
-                            <li><a href="#">Advertise with us</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="binduz-er-footer-logo">
-            <a href="#">
-                <img src="{{asset('site/images/logo-5.png')}}" alt="">
-            </a>
-        </div>
-    </div>
-
-    <!--====== BINDUZ FOOTER 3  PART ENDS ======-->
-
-    <!--====== BINDUZ BACK TO TOP PART START ======-->
-
-    <div class="binduz-er-back-to-top">
-        <p>BACK TO TOP <i class="fal fa-long-arrow-right"></i></p>
-    </div>
-
-    <!--====== BINDUZ BACK TO TOP PART ENDS ======-->
-
-
-
 
 
 
