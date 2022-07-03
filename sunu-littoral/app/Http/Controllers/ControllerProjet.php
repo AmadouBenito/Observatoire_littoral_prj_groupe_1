8f2e13e807cbe2e9a52590e8f775a7498ee3cfdb
RiieMa <marydiouf09@gmail.com>
2022-07-03 13:35:32 +0200

    appelOffre

--- sunu-littoral/app/Http/Controllers/ControllerProjet.php
+++ sunu-littoral/app/Http/Controllers/ControllerProjet.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appeldoffre;
 use App\Models\Fichier;
 use App\Models\TypeFichier;
 use Illuminate\Http\Request;
@@ -11,8 +12,30 @@ class ControllerProjet extends Controller
 {
     public function appelOffre()
     {
-        return view('site.projet.appelOffre');
+        // return view('site.projet.appelOffre');
+        $appoffre = Appeldoffre::all();
+        $nb_fichiers = count($appoffre);
+        $nb_offre = 0;
+        $url_appOffre = array();
+        $dateDeb = array();
+        $dateFin = array();
+        $libelle = array();
+        for ($i = 0; $i < $nb_fichiers; $i++) {
+            $dateDeb[] = $appoffre[$i]->dateDebut;
+            $dateFin[] = $appoffre[$i]->dateFin;
+            $libelle[] = $appoffre[$i]->libelle;
+            $fichier = $appoffre[$i]->fichier;
+            $url_appOffre[] = Storage::url($fichier);
+            echo $url_appOffre[$i];
+            $nb_offre++;
+        }
+
+
+
+
+        return view('site.projet.appelOffre', ['url_appOffre' => $url_appOffre, 'dateDeb' => $dateDeb, 'dateFin' => $dateFin, 'libelle' => $libelle, 'nb_offre' => $nb_offre]);
     }
+
 
 
     public function projet()
--- sunu-littoral/resources/views/site/projet/appelOffre.blade.php
+++ sunu-littoral/resources/views/site/projet/appelOffre.blade.php
@@ -3,4 +3,31 @@
 
 @section('appelOffre')
 
+<div class="row">
+
+    @for($i=0; $i<$nb_offre; $i++)
+    <div class="col-lg-4">
+      <div class="binduz-er-video-post recently-viewed-item">
+          <div class="binduz-er-latest-news-item">
+              <div class="binduz-er-thumb">
+              
+                    <iframe src="{{ $url_appOffre[$i] }}"></iframe>
+                    <p> {{ $url_appOffre[$i] }}</p>
+              </div>
+              <div class="binduz-er-content">
+                  <div class="binduz-er-meta-item">
+
+                      <div class="binduz-er-meta-date">
+                          <span><i class="fal fa-calendar-alt"></i>{{ $dateDeb[$i] }}</span>
+                          <span><i class="fal fa-calendar-alt"></i>{{ $dateFin[$i] }}</span>
+                      </div>
+                  </div>
+                  <h5 class="binduz-er-title">{{ $libelle[$i] }}</h5>
+              </div>
+          </div>
+      </div>
+     </div>
+    @endfor
+  </div>
+
 @endsection
