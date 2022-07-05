029c6a05e8a5039e888d72403fdf6e78285f789c
RiieMa <marydiouf09@gmail.com>
2022-07-05 22:20:54 +0200

    telechargement appel d offre

--- sunu-littoral/resources/views/site/projet/appelOffre.blade.php
+++ sunu-littoral/resources/views/site/projet/appelOffre.blade.php
@@ -10,19 +10,20 @@
         <div class="binduz-er-latest-news-item">
          
           
-                      <img  src="{{asset('site/images/iconePdf.jpg')}}" width="80" height="80" alt=""/>
-                      <span class="binduz-er-title"> {{ $libelle[$i] }}</span>
+                    <img  src="{{asset('site/images/iconePdf.jpg')}}" width="80" height="80" alt=""/>
+                    <span class="binduz-er-title"> {{ $libelle[$i] }}</span>
                      
-                      <div class="binduz-er-meta-date">
+                    <div class="binduz-er-meta-date">
                         <span><i class="fal fa-calendar-alt" ></i>cloture:{{ $dateFin[$i] }}</span>
                     </div>
                  
                    
                 <a href="{{ route('appelOffre.postuler' , [$fichier[$i] , $appelOffre_id[$i]] ) }}"> <button type="button" class="btn btn-outline-warning">Postuler</button></a>
-
+                <a href="{{ route('projet.telecharger',['id' => $appelOffre_id[$i]]) }}"><button type="button" class="btn btn-outline-success">Telecharger</button></a>
+   
             
-    </div>
-    </div>
+        </div>
+       </div>
     </div>
     @endfor
   </div>
