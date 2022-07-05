3c68ab1c78ca547206e60a7218ed4b95df32e4e6
elomega20 <elsedy000@gmail.com>
2022-07-05 15:31:31 -0400

    routes

--- sunu-littoral/routes/web.php
+++ sunu-littoral/routes/web.php
@@ -46,7 +46,7 @@ Route::view('/actualite', 'site.actualite'); //vue actualite
 //Page Proge et Appel d'offre
 Route::get('/projet', [ControllerProjet::class, 'projet'])->name('accueil.projet');
 Route::get('/appelOffre', [ControllerProjet::class, 'appelOffre'])->name('accueil.appelOffre');
-Route::get('/downloadProjet/{id}', [ControllerProjet::class, 'downloadProjet'])->name('projet.download');
+Route::get('/projet/telecharger{id}', [ControllerProjet::class, 'telecharger_projet'])->name('projet.telecharger');
 
 
 require __DIR__ . '/auth.php';
